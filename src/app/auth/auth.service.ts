6e7e62acf03e651550e028f367d8f7b08f339685
berkayozay <berkayyozayy@gmail.com>
2021-04-29 08:15:25 +0300

    code cleanup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth'
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  newUser: any
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) { }
+
+  createUser(user) {
+
+  }
 }
